7b3e216a328493aaf55462c6c408c643b1b4f953
mehdinathani <mehdinathani@gmail.com>
2023-11-11 13:32:59 +0500

    final update multi select optins

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -15,23 +15,45 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  List<String> _imagePaths = [];
   bool showActionButtons = false;
   String? _imagePath;
+  late bool isMultipleImages;
+
   @override
   void initState() {
     super.initState();
+    isMultipleImages = false;
     widget.viewModel.init(context);
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget displayImages() {
+      if (isMultipleImages) {
+        return ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: _imagePaths.length,
+          itemBuilder: (context, index) {
+            return Image.file(
+                File(_imagePaths[index])); // Displaying each image
+          },
+        );
+      } else {
+        return _imagePath != null
+            ? Image.file(File(_imagePath!)) // Displaying the single image
+            : Container(); // Placeholder or empty container if no image to show
+      }
+    }
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text('Document Scanner by MehdiNathani'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -41,18 +63,35 @@ class _HomeViewState extends State<HomeView> {
               children: [
                 CustomButton(
                   onPressed: () async {
-                    String? imagePath =
-                        await widget.viewModel.getImageFromCamera();
-                    if (imagePath != null) {
-                      showActionButtons = true;
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text("Success: $imagePath"),
-                        ),
-                      );
-                      setState(() {
-                        _imagePath = imagePath;
-                      });
+                    if (isMultipleImages) {
+                      List<String>? imagePath =
+                          await widget.viewModel.getImagesFromCameraMulti();
+                      if (imagePath != null) {
+                        showActionButtons = true;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Success: $imagePath"),
+                          ),
+                        );
+                        setState(() {
+                          _imagePaths.addAll(imagePath);
+                          _imagePath = imagePath.first;
+                        });
+                      }
+                    } else {
+                      String? imagePath =
+                          await widget.viewModel.getImageFromCamera();
+                      if (imagePath != null) {
+                        showActionButtons = true;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Success: $imagePath"),
+                          ),
+                        );
+                        setState(() {
+                          _imagePath = imagePath;
+                        });
+                      }
                     }
                   },
                   buttonText: "Scan",
@@ -63,18 +102,35 @@ class _HomeViewState extends State<HomeView> {
                 ),
                 CustomButton(
                   onPressed: () async {
-                    String? imagePath =
-                        await widget.viewModel.getImageFromGallery();
-                    if (imagePath != null) {
-                      showActionButtons = true;
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text("Success: $imagePath"),
-                        ),
-                      );
-                      setState(() {
-                        _imagePath = imagePath;
-                      });
+                    if (isMultipleImages) {
+                      List<String>? imagePath =
+                          await widget.viewModel.getImagesFromGalleryMulti();
+                      if (imagePath != null) {
+                        showActionButtons = true;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Success: $imagePath"),
+                          ),
+                        );
+                        setState(() {
+                          _imagePaths.addAll(imagePath);
+                          _imagePath = imagePath.first;
+                        });
+                      }
+                    } else {
+                      String? imagePath =
+                          await widget.viewModel.getImageFromGallery();
+                      if (imagePath != null) {
+                        showActionButtons = true;
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Success: $imagePath"),
+                          ),
+                        );
+                        setState(() {
+                          _imagePath = imagePath;
+                        });
+                      }
                     }
                   },
                   buttonText: "Upload from Gallery",
@@ -85,60 +141,147 @@ class _HomeViewState extends State<HomeView> {
             const SizedBox(height: 20),
             Visibility(
               visible: showActionButtons,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              child: Column(
                 children: [
-                  CustomButton(
-                    buttonText: "Share",
-                    buttonicon: Icons.share,
-                    onPressed: () async {
-                      await widget.viewModel.shareImage();
-                      if (widget.viewModel.shareSuccess) {
-                        SnackBarWidget.showSnackBar(
-                            context, "Image Shared Successfully.");
-                      }
-                    },
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CustomButton(
+                        buttonText: "Share",
+                        buttonicon: Icons.share,
+                        onPressed: () async {
+                          await widget.viewModel.shareImage();
+                          if (widget.viewModel.shareSuccess) {
+                            SnackBarWidget.showSnackBar(
+                                context, "Image Shared Successfully.");
+                          }
+                        },
+                      ),
+                      CustomButton(
+                        buttonicon: Icons.save_alt_outlined,
+                        buttonText: "Save to Gallery",
+                        onPressed: () async {
+                          await widget.viewModel.saveImage();
+                          if (widget.viewModel.saveSuccess) {
+                            SnackBarWidget.showSnackBar(
+                                context, 'Image saved successfully');
+                          }
+                        },
+                      ),
+                    ],
                   ),
-                  CustomButton(
-                    buttonicon: Icons.save_alt_outlined,
-                    buttonText: "Save to Gallery",
-                    onPressed: () async {
-                      await widget.viewModel.saveImage();
-                      if (widget.viewModel.saveSuccess) {
-                        SnackBarWidget.showSnackBar(
-                            context, 'Image saved successfully');
-                      }
-                    },
-                  )
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CustomButton(
+                          onPressed: () async {
+                            await widget.viewModel.convertImageToPDF();
+                          },
+                          buttonicon: Icons.picture_as_pdf,
+                          buttonText: "Save as PDF"),
+                      CustomButton(
+                        onPressed: () async {
+                          await widget.viewModel.sharePDF();
+                        },
+                        buttonicon: Icons.share,
+                        buttonText: "Share as PDF",
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
                 ],
               ),
             ),
-            const SizedBox(height: 20),
-            const Text(
-              'Cropped image path:',
-              style: TextStyle(color: Colors.white),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Switch(
+                  activeColor: Colors.blue,
+                  inactiveThumbColor: Colors.white,
+                  inactiveTrackColor: Colors.white,
+                  value: isMultipleImages,
+                  onChanged: (value) {
+                    setState(() {
+                      isMultipleImages = value;
+                      debugPrint(
+                        isMultipleImages
+                            ? "Multi Image Selection Enabled"
+                            : "Multi Image Selection Disabled",
+                      );
+                    });
+                  },
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  isMultipleImages
+                      ? "Multi Image Selection Enabled"
+                      : "Multi Image Selection Disabled",
+                  style: const TextStyle(color: Colors.white),
+                )
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 0, left: 0, right: 0),
+            const SizedBox(height: 20),
+            const Visibility(
+              visible: false,
               child: Text(
-                _imagePath.toString(),
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 14, color: Colors.white),
+                'Cropped image path:',
+                style: TextStyle(color: Colors.white),
               ),
+            ),
+            Visibility(
+              visible: false,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 0, left: 0, right: 0),
+                child: Text(
+                  _imagePath.toString(),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 14, color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
             ),
             Expanded(
               child: SingleChildScrollView(
                 child: Visibility(
                   visible: _imagePath != null,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.file(
-                      File(_imagePath ?? ''),
-                    ),
-                  ),
+                  child: isMultipleImages
+                      ? displayImages() // Displays the list of images
+                      : Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white)),
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.file(
+                            File(_imagePath ?? ''),
+                          ),
+                        ),
                 ),
               ),
             ),
+            // Expanded(
+            //   child: SingleChildScrollView(
+            //     child: Visibility(
+            //       visible: _imagePath != null,
+            //       child:  Container(
+
+            //         decoration:
+            //             BoxDecoration(border: Border.all(color: Colors.white)),
+            //         padding: const EdgeInsets.all(8.0),
+            //         child: Image.file(
+            //           File(_imagePath ?? ''),
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // ),
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -155,7 +298,7 @@ class _HomeViewState extends State<HomeView> {
                             context, 'Thank you for sharing!');
                       }
                     },
-                    child: Row(
+                    child: const Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Icon(
@@ -170,14 +313,18 @@ class _HomeViewState extends State<HomeView> {
                       ],
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
-                  Text(
-                    "Copy rights reserved by mehdinathani 2023",
-                    style: TextStyle(color: Colors.white),
+                  const Center(
+                    child: Text(
+                      "Copyright © 2023 Mehdi Nathani. All rights reserved.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
                   ),
-                  Divider(color: Colors.white),
+                  const Divider(color: Colors.white),
                 ],
               ),
             ),
--- lib/viewmodel/homeview_model.dart
+++ lib/viewmodel/homeview_model.dart
@@ -1,18 +1,25 @@
-import 'package:docscanneredgedemo/components/custom_snackbar.dart';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:camera_gallery_image_picker/camera_gallery_image_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:multi_image_picker/multi_image_picker.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:edge_detection/edge_detection.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomeViewModel {
   String? _imagePath;
+  String? _pdfPath;
   BuildContext context;
   bool shareSuccess = false;
   bool saveSuccess = false;
   bool appShared = false;
+  List<Asset> selectedImages = [];
 
   HomeViewModel(this.context);
 
@@ -110,6 +117,140 @@ class HomeViewModel {
     if (result.status == ShareResultStatus.success) {
       debugPrint('Thank you for sharing!');
       appShared = true;
+    }
+  }
+
+  Future<void> convertImageToPDF() async {
+    final imagePath = _imagePath!;
+    final pdfPath = imagePath.replaceAll('.jpeg', '.pdf');
+
+    final pdf = pw.Document();
+
+    // Read image bytes from file
+    final imageBytes = File(imagePath).readAsBytesSync();
+
+    // Create a pw.MemoryImage from image bytes
+    final image = pw.MemoryImage(imageBytes);
+
+    // Add a page to the PDF with the image
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Image(image),
+          );
+        },
+      ),
+    );
+
+    // Save the PDF to a Uint8List
+    final Uint8List pdfBytes = await pdf.save();
+
+    // Write the Uint8List to the file
+    final File file = File(pdfPath);
+    await file.writeAsBytes(pdfBytes);
+
+    debugPrint('PDF generated successfully at ${file.path}');
+    _pdfPath = file.path;
+  }
+
+  Future<void> sharePDF() async {
+    await convertImageToPDF();
+    if (_pdfPath != null) {
+      final result = await Share.shareFilesWithResult([_pdfPath!],
+          text: 'Check out this image!');
+      if (result.status == ShareResultStatus.success) {
+        shareSuccess = true;
+      }
+      // await Share.shareFiles([_imagePath!], text: 'Check out this image!');
+    }
+  }
+
+  // Future<List<String>?> getImagesFromCameraMulti() async {
+  //   bool isCameraGranted = await Permission.camera.request().isGranted;
+  //   if (!isCameraGranted) {
+  //     isCameraGranted =
+  //         await Permission.camera.request() == PermissionStatus.granted;
+  //   }
+
+  //   if (!isCameraGranted) {
+  //     // Have not permission to camera
+  //     debugPrint("No Camera Permission");
+  //     return null;
+  //   }
+
+  //   try {
+  //     List<Asset> resultList = await MultiImagePicker.pickImages(
+  //       maxImages: 300,
+  //       enableCamera: true,
+  //     );
+
+  //     // Convert Asset objects to file paths
+  //     List<String> imagePaths =
+  //         resultList.map((asset) => asset.identifier ?? '').toList();
+
+  //     if (imagePaths.isNotEmpty) {
+  //       return imagePaths;
+  //     } else {
+  //       return null;
+  //     }
+  //   } catch (e) {
+  //     debugPrint(e.toString());
+  //     return null;
+  //   }
+  // }
+
+  // Future<List<String>?> getImagesFromGalleryMulti() async {
+  //   try {
+  //     List<Asset> resultList = await MultiImagePicker.pickImages(
+  //       maxImages: 300,
+  //       enableCamera: false, // Disable camera for gallery
+  //     );
+
+  //     // Convert Asset objects to file paths
+  //     List<String> imagePaths =
+  //         resultList.map((asset) => asset.identifier ?? '').toList();
+
+  //     if (imagePaths.isNotEmpty) {
+  //       return imagePaths;
+  //     } else {
+  //       return null;
+  //     }
+  //   } catch (e) {
+  //     debugPrint(e.toString());
+  //     return null;
+  //   }
+  // }
+
+  Future<List<String>?> getImagesFromCameraMulti() async {
+    List<String> imagePaths = [];
+
+    // Loop to capture images using the edge detection
+    while (true) {
+      String? imagePath =
+          await getImageFromCamera(); // Function to capture single image using edge detection
+      if (imagePath != null) {
+        imagePaths.add(imagePath);
+      } else {
+        break; // Break the loop if there's an issue or the user stops capturing images
+      }
+    }
+
+    if (imagePaths.isNotEmpty) {
+      return imagePaths;
+    } else {
+      return null; // Handle if there are no captured images
+    }
+  }
+
+  Future<List<String>?> getImagesFromGalleryMulti() async {
+    List<File> multipleImageFiles =
+        await CameraGalleryImagePicker.pickMultiImage();
+
+    if (multipleImageFiles.isNotEmpty) {
+      return multipleImageFiles.map((file) => file.path).toList();
+    } else {
+      return null;
     }
   }
 }
